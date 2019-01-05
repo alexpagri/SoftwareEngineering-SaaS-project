545fc30ef2e2ae6b7fd6d08e464cea3858f4321c
alexpagri <921193+alexpagri@users.noreply.github.com>
2019-01-06 01:21:33 +0200

    Revert "test 5 ..."
    This reverts commit 6dd5833d71559231e591e5739d442bd79dd1a7ac.

--- project/settings.py
+++ project/settings.py
@@ -14,6 +14,8 @@ import os
 import django_heroku
 import dj_database_url
 
+django_heroku.settings(locals())
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -124,4 +126,3 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-django_heroku.settings(locals())
